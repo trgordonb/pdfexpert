1aadf10418c9f90a2b0fd15947ed145f5a10daf0
trgordonb <gordoncheng@outlook.com>
2023-06-16 00:45:47 +0800

    change ingest logic

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ from llama_index.node_parser import SimpleNodeParser
 from llama_index.vector_stores import PineconeVectorStore
 from langchain.llms import AzureOpenAI
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.llms import AzureOpenAI
+from langchain.document_loaders import PDFPlumberLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 import gradio as gr
 
 from dotenv import load_dotenv, find_dotenv
@@ -42,10 +43,10 @@ def create_service_context():
 
     max_input_size = 4096
     num_outputs = 512
-    max_chunk_overlap = 20
-    chunk_size_limit = 600
+    max_chunk_overlap = 200
+    chunk_size_limit = 1000
 
-    prompt_helper = PromptHelper(max_input_size, num_outputs, chunk_size_limit=chunk_size_limit, chunk_overlap_ratio=0.01)
+    prompt_helper = PromptHelper(max_input_size, num_outputs, chunk_size_limit=chunk_size_limit, chunk_overlap_ratio=0.2)
     llm_predictor = LLMPredictor(llm=AzureOpenAI(temperature=0.5, deployment_name="ohopenai", model_name="gpt-35-turbo", max_tokens=num_outputs))
     embedding_llm = LangchainEmbedding(OpenAIEmbeddings(deployment='text-embedding-ada-002'),embed_batch_size=1)
     service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper, embed_model=embedding_llm)
@@ -57,9 +58,10 @@ def data_ingestion_indexing(directory_path):
 
     pinecone_index_name = os.environ['PINECONE_INDEX']
     if pinecone_index_name in pinecone.list_indexes():
-        pinecone_index = pinecone.Index(index_name=pinecone_index_name)
-        vector_store = PineconeVectorStore(pinecone_index=pinecone_index)
-        store_index = GPTVectorStoreIndex.from_vector_store(vector_store=vector_store, service_context=create_service_context())
+        pinecone.delete_index(pinecone_index_name)
+        #pinecone_index = pinecone.Index(index_name=pinecone_index_name)
+        #vector_store = PineconeVectorStore(pinecone_index=pinecone_index)
+        #store_index = GPTVectorStoreIndex.from_vector_store(vector_store=vector_store, service_context=create_service_context())
     if pinecone_index_name not in pinecone.list_indexes():
         pinecone.create_index(
             name=pinecone_index_name,
@@ -67,10 +69,13 @@ def data_ingestion_indexing(directory_path):
             metric="cosine"
         )
         pinecone_index = pinecone.Index(index_name=pinecone_index_name)
+        #directoryLoader = DirectoryLoader(directory_path, loader_cls=PDFPlumberLoader)
+        #rawDocs = directoryLoader.load()
+        #textSplitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        #documents = textSplitter.split_documents(rawDocs)
         documents = SimpleDirectoryReader(directory_path).load_data()
         parser = SimpleNodeParser()
         nodes = parser.get_nodes_from_documents(documents)
-        
         vector_store = PineconeVectorStore(pinecone_index=pinecone_index)
         storage_context = StorageContext.from_defaults(vector_store=vector_store)
         store_index = GPTVectorStoreIndex.from_documents(nodes, storage_context=storage_context,service_context=create_service_context())
