67fe0da97bb47c0919367c49f2b3dcde5a373eed
trgordonb <gordoncheng@outlook.com>
2023-06-18 16:58:13 +0800

    remove dependency on llama_index

--- main.py
+++ main.py
@@ -3,21 +3,11 @@ import logging
 import sys
 import pinecone
 from fastapi import FastAPI, status
-from llama_index import (
-    LLMPredictor,
-    LangchainEmbedding,
-    ServiceContext,
-    GPTVectorStoreIndex,
-    SimpleDirectoryReader,
-    PromptHelper,
-    StorageContext,
-)
-from llama_index.node_parser import SimpleNodeParser
-from llama_index.vector_stores import PineconeVectorStore
-from langchain.llms import AzureOpenAI
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.document_loaders import PDFPlumberLoader, DirectoryLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Pinecone
+from langchain.document_loaders import PyPDFLoader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.chat_models import AzureChatOpenAI
+from langchain.chains import RetrievalQA
 import gradio as gr
 
 from dotenv import load_dotenv, find_dotenv
@@ -38,55 +28,43 @@ def hello_world():
 def perform_healthcheck():
     return {'healthcheck': 'Everything OK!'}
 
-
-def create_service_context():
-
-    max_input_size = 4096
-    num_outputs = 512
-    max_chunk_overlap = 200
-    chunk_size_limit = 1000
-
-    prompt_helper = PromptHelper(max_input_size, num_outputs, chunk_size_limit=chunk_size_limit, chunk_overlap_ratio=0.2)
-    llm_predictor = LLMPredictor(llm=AzureOpenAI(temperature=0.5, deployment_name="ohopenai", model_name="gpt-35-turbo", max_tokens=num_outputs))
-    embedding_llm = LangchainEmbedding(OpenAIEmbeddings(deployment='text-embedding-ada-002'),embed_batch_size=1)
-    service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper, embed_model=embedding_llm)
-    
-    return service_context
-
-
 def data_ingestion_indexing(directory_path):
-
     pinecone_index_name = os.environ['PINECONE_INDEX']
+    embeddings = OpenAIEmbeddings(deployment='text-embedding-ada-002', chunk_size=1)
+
+    store_index = None
     if pinecone_index_name in pinecone.list_indexes():
-        pinecone.delete_index(pinecone_index_name)
-        #pinecone_index = pinecone.Index(index_name=pinecone_index_name)
-        #vector_store = PineconeVectorStore(pinecone_index=pinecone_index)
-        #store_index = GPTVectorStoreIndex.from_vector_store(vector_store=vector_store, service_context=create_service_context())
-    if pinecone_index_name not in pinecone.list_indexes():
+        store_index = Pinecone.from_existing_index(pinecone_index_name, embeddings)
+    elif pinecone_index_name not in pinecone.list_indexes():
         pinecone.create_index(
             name=pinecone_index_name,
             dimension=1536,
             metric="cosine"
         )
-        pinecone_index = pinecone.Index(index_name=pinecone_index_name)
-        #directoryLoader = DirectoryLoader(directory_path, loader_cls=PDFPlumberLoader)
-        #rawDocs = directoryLoader.load()
-        #textSplitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-        #documents = textSplitter.split_documents(rawDocs)
-        documents = SimpleDirectoryReader(directory_path).load_data()
-        parser = SimpleNodeParser()
-        nodes = parser.get_nodes_from_documents(documents)
-        vector_store = PineconeVectorStore(pinecone_index=pinecone_index)
-        storage_context = StorageContext.from_defaults(vector_store=vector_store)
-        store_index = GPTVectorStoreIndex.from_documents(nodes, storage_context=storage_context,service_context=create_service_context())
+        loader = PyPDFLoader(f"{directory_path}/OHTechExplained.pdf")
+        pages = loader.load_and_split()
+        embeddings = OpenAIEmbeddings(deployment='text-embedding-ada-002', chunk_size=1)
+        store_index = Pinecone.from_documents(pages, embeddings, index_name=pinecone_index_name)
         
     return store_index
 
 def data_querying(input_text):
+    llm = AzureChatOpenAI(
+        openai_api_key=os.getenv('OPENAI_API_KEY'),
+        openai_api_base=os.getenv('OPENAI_API_BASE'),
+        openai_api_version=os.getenv('OPENAI_API_VERSION'),
+        deployment_name='ohopenai',
+        temperature=0.0
+    )
 
-    response = index.as_query_engine().query(input_text)
-    
-    return response.response
+    qa = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=index.as_retriever()
+    )
+
+    result = qa.run(input_text)
+    return result
 
 iface = gr.Interface(fn=data_querying,
                      server_name="0.0.0.0",
